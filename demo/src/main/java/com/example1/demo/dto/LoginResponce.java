80dc183035fa7d34621dd6b98e6af310c0fdb474
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-16 19:44:04 +0530

    create the responce dto

---
+++ demo/src/main/java/com/example1/demo/dto/LoginResponce.java
@@ -0,0 +1,12 @@
+package com.example1.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponce {
+    private String token;
+}
